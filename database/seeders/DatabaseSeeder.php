ade6c19b2596ac15b54314022f7d1e7cb318c765
osama zayed <117484488+osama-zayed@users.noreply.github.com>
2024-05-12 04:12:27 +0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
-use App\Models\department;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,17 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {       
-        department::create([
-            "name"=>"hgjdfi",
-            "phone_number"=>"775561590",
-            
-        ]);
         User::create([
             'name' => 'admin',
-            'email' => 'admin@admin.com',
+            'username' => 'admin@admin.com',
             'password' => bcrypt('123123123'),
             'user_type' => 'admin',
-            "department_id" => "1",
         ]);
         
     }
